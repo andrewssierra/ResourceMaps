16650f32614b82b7018d336f5c601285cc9321b5
andrewssierra <andrewssierra@yahoo.com>
2019-09-19 17:24:25 -0700

    Adding zoom buttons

--- app/javascript/components/Map/index.js
+++ app/javascript/components/Map/index.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-import styled from 'styled-components';
+import React from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import styled from "styled-components";
 
 const Wrapper = styled.div`
-    width: ${props => props.width};
-    height: ${props => props.height};
+	width: ${props => props.width};
+	height: ${props => props.height};
 `;
 
 export default class Map extends React.Component {
-    componentDidMount() {
-        this.map = L.map('map', {
-            center: [38.380422, -101.342848],
-            zoom: 4,
-            zoomControl: false
-        });
+	componentDidMount() {
+		this.map = L.map("map", {
+			center: [38.380422, -101.342848],
+			zoom: 4,
+			zoomControl: true
+		});
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            detectRetina: true,
-            maxZoom: 20,
-            maxNativeZoom: 17
-        }).addTo(this.map);
-    }
+		L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+			detectRetina: true,
+			maxZoom: 20,
+			maxNativeZoom: 17
+		}).addTo(this.map);
+	}
 
-    render() {
-        return <Wrapper width="1280" height="720" id="map" />;
-    }
+	render() {
+		return <Wrapper width='1280' height='720' id='map' />;
+	}
 }
