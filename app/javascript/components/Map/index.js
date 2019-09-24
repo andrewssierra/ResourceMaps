2a79c466f310900117fd6486c4428d97d23c098c
andrewssierra <andrewssierra@yahoo.com>
2019-09-23 22:32:50 -0700

    Changing map type to match the mockup.

--- app/javascript/components/Map/index.js
+++ app/javascript/components/Map/index.js
@@ -1,29 +1,28 @@
-import React from "react";
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
-import styled from "styled-components";
-
-const Wrapper = styled.div`
-	width: ${props => props.width};
-	height: ${props => props.height};
-`;
+import React from 'react';
+import L from 'leaflet';
+import styles from './Map.css';
+import 'leaflet/dist/leaflet.css';
+import { watchFile } from 'fs';
 
 export default class Map extends React.Component {
-	componentDidMount() {
-		this.map = L.map("map", {
-			center: [38.380422, -101.342848],
-			zoom: 4,
-			zoomControl: true
-		});
+    componentDidMount() {
+        this.map = L.map('map', {
+            center: [38.380422, -101.342848],
+            zoom: 4,
+            zoomControl: true
+        });
 
-		L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-			detectRetina: true,
-			maxZoom: 20,
-			maxNativeZoom: 17
-		}).addTo(this.map);
-	}
+        L.tileLayer(
+            'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',
+            {
+                detectRetina: true,
+                maxZoom: 20,
+                maxNativeZoom: 17
+            }
+        ).addTo(this.map);
+    }
 
-	render() {
-		return <Wrapper width='1280' height='720' id='map' />;
-	}
+    render() {
+        return <div id="map"></div>;
+    }
 }
