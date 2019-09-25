76721969a29f233b9869741c089e70d88fac77d5
andrewssierra <andrewssierra@yahoo.com>
2019-09-24 23:21:36 -0700

    Adding grid layout.

--- app/javascript/components/Map/index.js
+++ app/javascript/components/Map/index.js
@@ -1,29 +1,44 @@
-import React from "react";
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
-import styled from "styled-components";
+import React from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import styled from 'styled-components';
 
 const Wrapper = styled.div`
-	width: ${props => props.width};
-	height: ${props => props.height};
+    width: ${props => props.width};
+    height: ${props => props.height};
+    float: ${props => props.float};
+    left: ${props => props.left};
+    top: ${props => props.top};
 `;
 
 export default class Map extends React.Component {
-	componentDidMount() {
-		this.map = L.map("map", {
-			center: [38.380422, -101.342848],
-			zoom: 4,
-			zoomControl: true
-		});
+    componentDidMount() {
+        this.map = L.map('map', {
+            center: [46.996085, -121.077026],
+            zoom: 8,
+            zoomControl: true
+        });
 
-		L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-			detectRetina: true,
-			maxZoom: 20,
-			maxNativeZoom: 17
-		}).addTo(this.map);
-	}
+        L.tileLayer(
+            'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',
+            {
+                detectRetina: true,
+                maxZoom: 20,
+                maxNativeZoom: 17
+            }
+        ).addTo(this.map);
+    }
 
-	render() {
-		return <Wrapper width='1280' height='720' id='map' />;
-	}
+    render() {
+        return (
+            <Wrapper
+                width="calc(100% - 18px)"
+                height="calc(100% - 18px)"
+                left="9px"
+                top="9px"
+                float="center"
+                id="map"
+            />
+        );
+    }
 }
--- app/javascript/components/app.js
+++ app/javascript/components/app.js
@@ -1,10 +1,70 @@
 import React from 'react';
 import Map from './Map/index';
+import GridLayout from 'react-grid-layout';
+import RGL, { WidthProvider } from 'react-grid-layout';
+import './Map/index.css';
 
-class App extends React.Component {
+const ReactGridLayout = WidthProvider(RGL);
+
+class MyFirstGrid extends React.Component {
     render() {
-        return <Map />;
+        return (
+            <div style={{ backgroundColor: '#141414' }}>
+                <GridLayout
+                    className="layout"
+                    cols={12}
+                    rowHeight={37}
+                    width={1900}
+                    isResizable={true}
+                >
+                    <div
+                        key="b"
+                        data-grid={{
+                            x: 0,
+                            y: 0,
+                            w: 6,
+                            h: 10,
+                            minW: 2,
+                            maxW: 8
+                        }}
+                        style={{
+                            backgroundColor: '#1f1d1d'
+                        }}
+                    >
+                        b
+                    </div>
+                    <div
+                        key="c"
+                        data-grid={{ x: 4, y: 6, w: 12, h: 10 }}
+                        style={{
+                            backgroundColor: '#1f1d1d',
+                            resize: 'both',
+                            overflow: 'auto'
+                        }}
+                    >
+                        c
+                    </div>
+                    <div
+                        key="a"
+                        data-grid={{
+                            x: 6,
+                            y: 0,
+                            w: 6,
+                            h: 10,
+                            minW: 2,
+                            maxW: 8
+                        }}
+                        style={{
+                            backgroundColor: '#1f1d1d',
+                            resize: 'both',
+                            overflow: 'auto'
+                        }}
+                    >
+                        <Map />
+                    </div>
+                </GridLayout>
+            </div>
+        );
     }
 }
-
-export default App;
+export default MyFirstGrid;
